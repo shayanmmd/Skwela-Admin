e043b9fbff7e79f04ca3dab8097e51eb3b89c490
Shayan Mehmandoost <shayanmehmandoost37@yahoo.com>
2024-08-31 13:45:53 +0330

    commit

--- modules/User/Http/Models/User.php
+++ modules/User/Http/Models/User.php
@@ -4,10 +4,12 @@ namespace User\Http\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory;
+    use HasRoles;
 
     public $guarded = [];
 }
